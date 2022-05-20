c30c40c83b324c8c30b7172b65ee78a7d3a6b434
Joel16 <joel16dev@gmail.com>
2022-05-19 20:22:45 -0400

    kumdman: Clean-up and fix indentation

--- kumdman/main.c
+++ kumdman/main.c
@@ -8,23 +8,23 @@ int sceUmdManGetUmdDrive(int driveNum);
 int sceUmdExecInquiryCmd(void *drive, u8 *param, u8 *buf);
 
 void *pspUmdManGetUmdDrive(int driveNum) {
-	int k1 = pspSdkSetK1(0);
-	void *drive = (void *)sceUmdManGetUmdDrive(driveNum);
-	pspSdkSetK1(k1);
-	return drive;
+    int k1 = pspSdkSetK1(0);
+    void *drive = (void *)sceUmdManGetUmdDrive(driveNum);
+    pspSdkSetK1(k1);
+    return drive;
 }
 
 int pspUmdExecInquiryCmd(void *drive, u8 *param, u8 *buf) {
-	int k1 = pspSdkSetK1(0);
-	int res = sceUmdExecInquiryCmd(drive, param, buf);
-	pspSdkSetK1(k1);
-	return(res);
+    int k1 = pspSdkSetK1(0);
+    int res = sceUmdExecInquiryCmd(drive, param, buf);
+    pspSdkSetK1(k1);
+    return(res);
 }
 
 int module_start(SceSize args __attribute__((unused)), void *argp __attribute__((unused))) {
-	return 0;
+    return 0;
 }
 
 int module_stop(void) {
-	return 0;
+    return 0;
 }
